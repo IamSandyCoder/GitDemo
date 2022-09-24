6f9f7266a3211f6c66ac463c69bc67a1cc76c43a
IamSandyCoder <ssnrage@gmail.com>
2022-09-24 14:48:43 +0530

    NEW1

--- pythonSelenium/framesDemo.py
+++ pythonSelenium/framesDemo.py
@@ -15,6 +15,14 @@ print(driver.find_element(By.CSS_SELECTOR,"h3").text)
 
 
 
+driver.get("https://the-internet.herokuapp.com/iframe")
+driver.switch_to.frame("mce_0_ifr")
+driver.find_element(By.ID,"tinymce").clear()
+driver.find_element(By.ID,"tinymce").send_keys("I am able to automate frames")
+driver.switch_to.default_content()
+print(driver.find_element(By.CSS_SELECTOR,"h3").text)
+
+
 
 
 
